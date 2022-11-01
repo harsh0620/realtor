e172b4b2ff7cdaa778e715c0bc4d017ea81736c5
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-11-02 00:13:39 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
