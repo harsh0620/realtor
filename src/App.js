5da5a3c76e1581c741488845a66933475198e724
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-11-02 12:11:35 +0530

    Created privateRouted , customHooks, Logout Functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
@@ -18,7 +19,9 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/offers" element={<Offers />} />
         </Routes>
         <ToastContainer
