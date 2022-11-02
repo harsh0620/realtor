dd50f2fa97ebdfa571fbdffd2c0e03efcd180ef7
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-11-02 15:06:08 +0530

    Spinner added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
     });
   }, [auth]);
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto ">
         <div>
           <img
